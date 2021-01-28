2edeeccb22d6dc000223f8d22041d5ae7243d1b4
Nikolina Pelivan <nikolina_pelivan@hotmail.com>
2021-01-28 10:56:25 +0100

    Desktop features finished

--- src/components/main.js
+++ src/components/main.js
@@ -5,6 +5,9 @@ import Firefox from "./images/logo-firefox.svg";
 import Opera from "./images/logo-opera.svg";
 import Dots from "./images/bg-dots.svg";
 import Arrow from "./images/icon-arrow.svg";
+import Tab1 from "./images/illustration-features-tab-1.svg";
+import Tab2 from "./images/illustration-features-tab-2.svg";
+import Tab3 from "./images/illustration-features-tab-3.svg";
 import "./style.css";
 import Tabs from "./Tabs";
 export default class Main extends Component {
@@ -39,33 +42,51 @@ export default class Main extends Component {
           </div>
           <Tabs>
             <div label="Simple Bookmarking">
-              <h2>Bookmark in one click</h2>
-              <p>
-                Organize your bookmarks however you like. Our simple
-                drag-and-drop interface gives you complete control over how you
-                manage your favourite sites.
-              </p>
-              <button>More info</button>
+              <div className="tab-item">
+                <img src={Tab1} alt="Tab image" />
+                <div>
+                  <h2>Bookmark in one click</h2>
+                  <p>
+                    Organize your bookmarks however you like. Our simple
+                    drag-and-drop interface gives you complete control over how
+                    you manage your favourite sites.
+                  </p>
+                  <button>More info</button>
+                </div>
+              </div>
             </div>
 
             <div label="Speedy Searching">
-              <h2>Intelligent search</h2>
-              <p>
-                Our powerful search feature will help you find saved sites in no
-                time at all. No need to trawl through all of your bookmarks.
-              </p>
-              <button>More info</button>
+              <div className="tab-item">
+                <img src={Tab2} alt="Tab image" />
+                <div>
+                  <h2>Intelligent search</h2>
+                  <p>
+                    Our powerful search feature will help you find saved sites
+                    in no time at all. No need to trawl through all of your
+                    bookmarks.
+                  </p>
+                  <button>More info</button>
+                </div>
+              </div>
             </div>
 
-            <div label="Easy Sharing">
-              <h2>Share your bookmarks</h2>
-              <p>
-                Easily share your bookmarks and collections with others. Create
-                a shareable link that you can send at the click of a button.
-              </p>
-              <button>More info</button>
+            <div label="Easy Sharing" className="tab-item">
+              <div className="tab-item">
+                <img src={Tab3} alt="Tab image" />
+                <div>
+                  <h2>Share your bookmarks</h2>
+                  <p>
+                    Easily share your bookmarks and collections with others.
+                    Create a shareable link that you can send at the click of a
+                    button.
+                  </p>
+                  <button>More info</button>
+                </div>
+              </div>
             </div>
           </Tabs>
+          <div className="left-blue-line"></div>
         </section>
 
         <section>
